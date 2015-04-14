4a68bb5587030c1e0c3c5999fcb8aed3e6fe5bcd
Lars Tangvald <lars.tangvald@oracle.com>
2015-04-14 13:58:49 +0200

    Various fixes.
    * Removed unnecessary folder check
    * Specify pid-file for 5.5 instead of using hostname
    * Change server shutdown check to be more reliable (socket is closed before all resources are released, which could lead to issues)

--- 5.7/docker-entrypoint.sh
+++ 5.7/docker-entrypoint.sh
@@ -27,9 +27,8 @@ if [ "$1" = 'mysqld' ]; then
 			echo >&2 '  Did you forget to add -e MYSQL_ROOT_PASSWORD=... ?'
 			exit 1
 		fi
-		if [ ! -d "$DATADIR" ]; then
-			mkdir -p "$DATADIR"
-		fi
+
+		mkdir -p "$DATADIR"
 		chown -R mysql:mysql "$DATADIR"
 
 		echo 'Initializing database'
@@ -64,7 +63,7 @@ if [ "$1" = 'mysqld' ]; then
 		EOSQL
 
 		if [ "$MYSQL_DATABASE" ]; then
-			echo "CREATE DATABASE IF NOT EXISTS \`"$MYSQL_DATABASE"\` ;" >> "$tempSqlFile"
+			echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` ;" >> "$tempSqlFile"
 		fi
 
 		if [ "$MYSQL_USER" -a "$MYSQL_PASSWORD" ]; then
@@ -81,7 +80,7 @@ if [ "$1" = 'mysqld' ]; then
 		rm -f "$tempSqlFile"
 		kill $(cat $PIDFILE)
 		for i in $(seq 30 -1 0); do
-			[ -S $SOCKET ] || break
+			[ -f "$PIDFILE" ] || break
 			echo 'MySQL init process in progress...'
 			sleep 1
 		done
